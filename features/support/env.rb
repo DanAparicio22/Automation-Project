4c4db3a28387bf64f0f662ddc54cd05b03731956
Bruno Mendieta <brn.mndt.cbllr@gmail.com>
2018-06-07 22:11:32 -0400

    added administrator feature and steps, increased wait time to 20

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,13 +6,16 @@ require 'capybara/cucumber'
 ENV['USERCOMPANY']="company@mail.com"
 ENV['PSWCOMPANY']="Company1"
 
+ENV['USERADMIN']="Administrador.bolsadetrabajoucb@gmail.com"
+ENV['PSWADMIN']="123456Ab"
+
 Capybara.default_driver = :selenium
 
 # Set the host the Capybara tests should be run against
 Capybara.app_host = ENV["CAPYBARA_HOST"]
 
 # Set the time (in seconds) Capybara should wait for elements to appear on the page
-Capybara.default_max_wait_time = 15
+Capybara.default_max_wait_time = 20
 Capybara.default_driver = :selenium
 Capybara.app_host = "http://bolsadetrabajo.ucbcba.edu.bo/"
 
